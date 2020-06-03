67aef9a6d4fba7e98b498e69fa4e4ab81e60141e
Your Name <you@example.com>
2020-06-03 21:26:05 +0700

    aaa

--- src/views/IndexPage/Sections/VForm.js
+++ src/views/IndexPage/Sections/VForm.js
@@ -163,7 +163,7 @@ export default function Admis_Form(props) {
 
 	useEffect(() => {
 		async function fetchData() {
-			const requestUrl = 'http://127.0.0.1:8000/api/nganh';
+			const requestUrl = 'http://ttts.local/public/api/nganh';
 			const response = await fetch(requestUrl);
 			const responseJSON = await response.json();
 
@@ -282,13 +282,13 @@ export default function Admis_Form(props) {
 				linkImage: ""
 			}
 			admissionsRecords.linkImage = res;
-			console.log(admissionsRecords);
+			console.log(JSON.stringify( admissionsRecords ));
 			const requestOptions = {
 				method: 'POST',
 				headers: { 'Content-Type': 'application/json' },
 				body: JSON.stringify({ admissionsRecords: admissionsRecords })
 			};
-			const response = await fetch('http://127.0.0.1:8000/api/luuhoso', requestOptions);
+			const response = await fetch('http://ttts.local/public/api/luuhoso', requestOptions);
 			const data = await response.json();
 			console.log(data);
 		})
